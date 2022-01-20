9ec3bfbc21205b4a51611306650020993df59bcf
Angel-Malek <anami.study@gmail.com>
2022-01-20 22:33:08 +0100

    cleaning code

--- src/main/java/com/example/sportradar/SportRadarApplication.java
+++ src/main/java/com/example/sportradar/SportRadarApplication.java
@@ -23,16 +23,16 @@ public class SportRadarApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
 
-        EventList eventList = objectMapper.readValue(new File("/Users/angel/IdeaProjects/SportRadar/src/main/resources/data.json"), EventList.class );
+        EventList eventList = objectMapper.readValue(new File("/Users/angel/IdeaProjects/SportRadar/src/main/resources/data.json"), EventList.class);
 
 //        System.out.println(event.getCompetition_name());
 
-        System.out.println( eventList.getEvent().get(0).getProbability_away_team_winner());
+        System.out.println(eventList.getEvent().get(0).getProbability_away_team_winner());
 
 
         ArrayList<Double> chanseContainer = new ArrayList<Double>();
 
-        for (int i = 0; i < eventList.getEvent().size()  ;  i++){
+        for (int i = 0; i < eventList.getEvent().size(); i++) {
             chanseContainer.add(eventList.getEvent().get(i).getProbability_home_team_winner());
             chanseContainer.add(eventList.getEvent().get(i).getProbability_draw());
             chanseContainer.add(eventList.getEvent().get(i).getProbability_away_team_winner());
@@ -45,11 +45,10 @@ public class SportRadarApplication implements CommandLineRunner {
         System.out.println(eventList.getEvent().get(0).getProbability_draw());
 
 
-
         System.out.println(chanseContainer);
 
-        for (int i  =0; i< eventList.getEvent().size()-1; i++ ){
-            for (int j = 0; j < 10 ; j++) {
+        for (int i = 0; i < eventList.getEvent().size() - 1; i++) {
+            for (int j = 0; j < 10; j++) {
                 if (eventList.getEvent().get(i).getProbability_away_team_winner() == chanseContainer.get(j)
                         || eventList.getEvent().get(i).getProbability_draw() == chanseContainer.get(j)
                         || eventList.getEvent().get(i).getProbability_home_team_winner() == chanseContainer.get(j)) {
@@ -57,13 +56,6 @@ public class SportRadarApplication implements CommandLineRunner {
                 }
             }
         }
-
-
-
-
-
-
-
 
 
     }
